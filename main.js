098f6e783223be6aa25fa642b5802506a81e5b0a
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-01-20 15:35:40 -0500

    resumed slideToggle with css override

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ $(function() {
   }) // offsets anchor by 75px (size of the header) and adds in scroll animation
 
   $('.burger-btn, .nav-link').on('click', event => {
-    $('.nav-list').toggleClass('open')
+    // $('.nav-list').toggleClass('open')
+    $('.nav-list').slideToggle(250)
   })
 }) //end of document ready
