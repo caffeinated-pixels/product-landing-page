8eb1743cd6b0153519281c8e2050901a097e87c4
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-01-21 11:10:31 -0500

    jQ now checks window size & rm style attr from .nav-list

--- main.js
+++ main.js
@@ -10,8 +10,18 @@ $(function() {
     )
   }) // offsets anchor by 75px (size of the header) and adds in scroll animation
 
+  $(window).resize(() => {
+    if ($(window).width() > 1024) {
+      $('.nav-list').removeAttr('style')
+    }
+  })
+
   $('.burger-btn, .nav-link').on('click', event => {
     // $('.nav-list').toggleClass('open')
-    $('.nav-list').slideToggle(250)
+    event.preventDefault()
+
+    if ($(window).width() <= 1024) {
+      $('.nav-list').slideToggle(250)
+    }
   })
 }) //end of document ready
